bc15a696cf14f2586443dee1a987ef8358d73662
miracle-man <62384215+miracle-man@users.noreply.github.com>
2021-11-13 22:44:34 +0300

    auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const JWT_SECRET = 'super-secret-key';
+const { JWT_SECRET, NODE_ENV } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Не выполнена авторизация'));
+    next(new UnauthorizedError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-secret-key');
   } catch (err) {
     next(new UnauthorizedError('JWT не прошел проверку'));
   }
