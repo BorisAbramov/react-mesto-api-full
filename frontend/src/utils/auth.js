6f1e8e778d847c193d3156180852f93728118284
miracle-man <62384215+miracle-man@users.noreply.github.com>
2021-11-14 20:33:26 +0300

    front7

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -37,7 +37,7 @@ export const login = (email, password) => {
              Accept: "application/json",
              "Content-Type": "application/json",
            },
-    body: JSON.stringify( email, password ),
+    body: JSON.stringify({ email, password }),
   }).then((res) => {
     return checkStatus(res);
   })
