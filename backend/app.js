67c178bb5ff2b0ec4f77943a24d082dd6fce3362
miracle-man <62384215+miracle-man@users.noreply.github.com>
2021-11-15 15:28:30 +0300

    rev1

--- backend/app.js
+++ backend/app.js
@@ -13,29 +13,9 @@ const BadRequestError = require('./errors/BadRequestError');
 const handleError = require('./errors/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// const options = {
-//   origin: [
-//     'http://localhost:3000',
-//     'https://http://mesto-full.nomoredomains.work',
-//     'https://BorisAbramov.github.io',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
-
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,
-//   Content-Type, Accept, Authorization');
-//   next();
-// });
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
